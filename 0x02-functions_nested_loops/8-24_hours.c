85f668154e374a607b79c766d2d10e194b19dad1
ApstPaul <idahosapaul07@gmail.com>
2022-10-19 12:24:07 -0700

    nested 119

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,9 +19,10 @@ void times_table(void)
 			prod = num * mult;
 
 			if (prod <= 9)
-				_Putchar(' ');
+				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
+
 			_putchar((prod % 10) + '0');
 		}
 		_putchar('\n');
