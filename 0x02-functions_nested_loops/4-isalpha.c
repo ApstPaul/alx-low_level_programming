64484abe83d197c7cf1c7da35bbb4ed77d7b00f6
ApstPaul <idahosapaul07@gmail.com>
2022-10-19 06:54:46 -0700

    nested 5

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,10 @@
 /**
  * _isalpha - checks for alphabets
  * @c: character is to be checked
+ *
  * Return: 1 if character is a letter, 0 if otherwise
  */
-int _isaplpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
