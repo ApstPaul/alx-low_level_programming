a39d82b23d25d0e5e8e0dcc23d974e801ad0ed38
ApstPaul <idahosapaul07@gmail.com>
2022-11-02 05:39:17 -0700

    recur0

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -3,9 +3,11 @@
 /**
  * _putchar - wtites the character c to stdout
  * @c: char to be printed
- * Return: Always 0 
+ *
+ * Return: 1 on succcess.
+ * on error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (0);
+	return (write(1, &c, 1));
 }
